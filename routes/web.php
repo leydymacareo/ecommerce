2b8a25e4250bb51a462d023140e839349196613a
Leydy Macareo <lmacareo456@unab.edu.co>
2025-10-16 11:54:58 -0500

    Se configuro el login

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,6 @@ Route::prefix('products')->controller(ProductController::class)->group(function 
     Route::get('/create', 'create')->name('products.create');     // /products/create
     Route::get('/{id}/{category?}', 'show')->name('products.show'); // /products/1/categoria
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
